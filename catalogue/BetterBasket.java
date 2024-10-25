c69e97d5b0b4018f65dd6d9e02f46331c67466d6
Pete <professorpicard@proton.me>
2024-10-25 21:03:58 +0100

    Revert "First Commit, Testing new branch"
    This reverts commit cb52b922ba35b036d730ec7936f14278a93f85ff.

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -14,5 +14,7 @@ public class BetterBasket extends Basket implements Serializable
   private static final long serialVersionUID = 1L;
   
 
-  // New Better Basket code will go here
+  // You need to add code here
+  // merge the items for same product,
+  // or sort the item based on the product number
 }
