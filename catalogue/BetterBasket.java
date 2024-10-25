cb52b922ba35b036d730ec7936f14278a93f85ff
Pete <professorpicard@proton.me>
2024-10-25 20:54:13 +0100

    First Commit, Testing new branch

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -14,7 +14,5 @@ public class BetterBasket extends Basket implements Serializable
   private static final long serialVersionUID = 1L;
   
 
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
+  // New Better Basket code will go here
 }
